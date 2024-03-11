35ca2aeaa86aa4bf7875361ec39e89d5926f18d7
Rianne Lim <rianne.limje@gmail.com>
2024-03-11 15:06:41 +0800

    Updated images to links
    Somehow image will only display for the address but not local file

--- src/views/ClassList.vue
+++ src/views/ClassList.vue
@@ -12,3 +12,16 @@ export default {
     <h1>Classes</h1>
     <CardBooker/>
 </template>
+
+<style scoped>
+body {
+  /* background-image: url('/images/image.png');
+  background-repeat: no-repeat;
+  background-size: cover; */ 
+  /* somehow the background image seems to be very blur so i just put it as black first */
+  background-color: black;
+}
+h1 {
+  color: #c7ff9c;
+}
+</style>
