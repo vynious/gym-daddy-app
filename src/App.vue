48154fd0537aa7966715d640b5377c6945d7361d
Rianne Lim <rianne.limje@gmail.com>
2024-03-16 13:45:35 +0800

    Revert "Merge Class into Main"

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
-
-<script>
-  import NavigationBar from './components/NavigationBar.vue'
-  export default {
-    components: {
-      NavigationBar
-    }
-  }
-</script>
-
 <template>
-  <div id="app">
-    <NavigationBar />
-    <router-view></router-view>
-  </div>
-
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/classList">Book</router-link> |
+    <router-link to="/queue">Queue</router-link> |
+    <router-link to="/profile">Profile</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <style>
 #app {
-  font-family: 'Poppins';
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: black;
+  color: #2c3e50;
 }
 
-body {
-  margin: 0;
-  padding: 0;
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,74 +1,62 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ClassList from '../views/ClassList.vue'
+import UserQueue from '../views/UserQueue.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/classList',
+    name: 'classList',
+    component: ClassList
+  },
+  {
+    path: '/queue',
+    name: 'queue',
+    component: UserQueue
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileDefaultPage.vue')
+  },
+  {
+      path: '/profileEditInfo',
+      name: 'profileEditInfo',
+      component: () => import('../views/ProfileEditInfoPage.vue')
+  },
+  {
+    path: '/profileBookings',
+    name: 'profileBookings',
+    component: () => import('../views/ProfileBookingsPage.vue')
+  },
+  {
+      path: '/profileRefer',
+      name: 'profileRefer',
+      component: () => import('../views/ProfileReferPage.vue')
+  },
+  {
+      path: '/profileLogout',
+      name: 'profileLogout',
+      component: () => import('../views/ProfileLogoutPage.vue')
+  },
+]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: () => import('../views/HomePage.vue')
-        },
-        {
-            path: '/queue',
-            name: 'queue',
-            component: () => import('../views/QueuePage.vue')
-        },
-
-        {
-            path: '/joinqueue',
-            name: 'joinqueue',
-            component: () => import('../views/JoinQueue.vue')
-        },
-
-        {   
-            path: '/book',
-            name: 'book',
-            component: () => import('../views/ClassList.vue')
-        },
-        {
-            path: '/bookyoga',
-            name: 'bookyoga',
-            component: () => import('../views/BookYoga.vue')
-
-        },
-        {
-            path: '/bookdance',
-            name: 'bookdance',
-            component: () => import('../views/BookDance.vue')
-
-        },
-        {
-            path: '/bookpilates',
-            name: 'bookpilates',
-            component: () => import('../views/BookPilates.vue')
-
-        },
-         {
-            path: '/profile',
-            name: 'profile',
-            component: () => import('../views/ProfileDefaultPage.vue')
-          },
-          {
-              path: '/profileEditInfo',
-              name: 'profileEditInfo',
-              component: () => import('../views/ProfileEditInfoPage.vue')
-          },
-          {
-            path: '/profileBookings',
-            name: 'profileBookings',
-            component: () => import('../views/ProfileBookingsPage.vue')
-          },
-          {
-              path: '/profileRefer',
-              name: 'profileRefer',
-              component: () => import('../views/ProfileReferPage.vue')
-          },
-          {
-              path: '/profileLogout',
-              name: 'profileLogout',
-              component: () => import('../views/ProfileLogoutPage.vue')
-          },
-    ]
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
