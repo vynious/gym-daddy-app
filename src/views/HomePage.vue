f44b48c27390820153baf63987562795d105dcb7
kkeona <chuakeona2003@gmail.com>
2024-03-17 20:20:04 +0800

    home update #1
    added 4 pngs to the assets folder
    
    added 1 component for the home page
    
    edited HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,33 @@
+<script>
+  import HomePageDesign from '@/components/HomePageDesign.vue'
+  export default {
+    components: {
+      HomePageDesign
+    }
+  }
+</script>
+
 <template>
-    this is the home page
+  <div id="app">
+    <HomePageDesign/>
+    <router-view></router-view>
+  </div>
 </template>
+
+<style scoped>
+/* #app {
+  font-family: 'Poppins';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+} */
+
+/* #app {
+    display: flex;
+    align-items: center;
+    /* background-image: url('../assets/background.png');
+    background-size : cover;
+    background-position: center; */
+/*} */
+</style>
