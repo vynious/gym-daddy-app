f1d794ba8e7a80ec163c78a2eebbfaa39994dab0
vynious <shawnthiah@gmail.com>
2024-03-18 21:34:54 +0800

    added docker compose and compose script

---
+++ build_compose.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Build Docker images
+echo "Building Docker images..."
+
+echo " "
+
+echo "Building Docker Queue CMS..."
+docker build -t gd-join-queue-cms gym-daddy-joinqueue-cms/.
+
+echo " "
+
+echo "Building Docker Notification MS..."
+docker build -t gd-notification-ms gym-daddy-notification-ms/.
+
+echo " "
+
+echo "Building Docker Queue MS..."
+docker build -t gd-queue-ms gym-daddy-queue-ms/.
+
+echo " "
+
+echo "Building Docker Telemessenger MS..."
+docker build -t gd-telemessager-ms gym-daddy-telemessenger-ms/.
+
+echo " "
+
+# Add more docker build commands for additional images if needed
+
+echo "Building Docker Log MS..."
+docker build -t gd-log-ms gym-daddy-log-ms/.
+
+
+echo " "
+
+echo "Building Docker Kong Gateway..."
+docker build -t gd-kong-gateway gym-daddy-kong-gateway/.
+
+
+echo " "
+
+# Run Docker Compose
+echo "Running Docker Compose..."
+docker-compose up -d
