737a788ebe476870ebb8dc5a77d1233cc4dedc8f
vynious <shawnthiah@gmail.com>
2024-03-23 19:32:23 +0800

    something

--- build_compose.sh
+++ build_compose.sh
@@ -6,35 +6,35 @@ echo "Building Docker images..."
 echo " "
 
 echo "Building Docker Queue CMS..."
-docker build -t gd-join-queue-cms gym-daddy-joinqueue-cms/.
+docker build -t gd-join-queue-cms gd-joinqueue-cms/.
 
 echo " "
 
 echo "Building Docker Notification MS..."
-docker build -t gd-notification-ms gym-daddy-notification-ms/.
+docker build -t gd-notification-ms gd-notification-ms/.
 
 echo " "
 
 echo "Building Docker Queue MS..."
-docker build -t gd-queue-ms gym-daddy-queue-ms/.
+docker build -t gd-queue-ms gd-queue-ms/.
 
 echo " "
 
 echo "Building Docker Telemessenger MS..."
-docker build -t gd-telemessager-ms gym-daddy-telemessenger-ms/.
+docker build -t gd-telemessenger-ms gd-telemessenger-ms/.
 
 echo " "
 
 # Add more docker build commands for additional images if needed
-
-echo "Building Docker Log MS..."
-docker build -t gd-log-ms gym-daddy-log-ms/.
+#
+#echo "Building Docker Log MS..."
+#docker build -t gd-log-ms gd-log-ms/.
 
 
 echo " "
 
 echo "Building Docker Kong Gateway..."
-docker build -t gd-kong-gateway gym-daddy-kong-gateway/.
+docker build -t gd-kong-gateway gd-kong-gateway/.
 
 
 echo " "
