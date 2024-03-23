61206928d14859ed3c2ba89b76c23afcddf273c9
ljlimjk <ljlimjk10@gmail.com>
2024-03-23 17:13:33 +0800

    fix path

--- build_compose.sh
+++ build_compose.sh
@@ -6,22 +6,22 @@ echo "Building Docker images..."
 echo " "
 
 echo "Building Docker Queue CMS..."
-docker build -t gd-join-queue-cms gym-daddy-joinqueue-cms/.
+docker build -t gd-join-queue-cms gd-joinqueue-cms
 
 echo " "
 
 echo "Building Docker Notification MS..."
-docker build -t gd-notification-ms gym-daddy-notification-ms/.
+docker build -t gd-notification-ms gd-notification-ms/.
 
 echo " "
 
 echo "Building Docker Queue MS..."
-docker build -t gd-queue-ms gym-daddy-queue-ms/.
+docker build -t gd-queue-ms gd-queue-ms/.
 
 echo " "
 
 echo "Building Docker Telemessenger MS..."
-docker build -t gd-telemessager-ms gym-daddy-telemessenger-ms/.
+docker build -t gd-telemessager-ms gd-telemessenger-ms/.
 
 echo " "
 
@@ -37,13 +37,13 @@ echo " "
 # Add more docker build commands for additional images if needed
 
 echo "Building Docker Log MS..."
-docker build -t gd-log-ms gym-daddy-log-ms/.
+docker build -t gd-log-ms gd-log-ms/.
 
 
 echo " "
 
 echo "Building Docker Kong Gateway..."
-docker build -t gd-kong-gateway gym-daddy-kong-gateway/.
+docker build -t gd-kong-gateway gd-kong-gateway/.
 
 echo " "
 
