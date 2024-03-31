f7ab9c7e4bc2985e3128703f4e73c7c481032399
ljlimjk <ljlimjk10@gmail.com>
2024-03-31 18:50:18 +0800

    enable flask-cors in classes ms

--- gd-classes-ms/docker/classes.py
+++ gd-classes-ms/docker/classes.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from os import environ
@@ -6,6 +7,7 @@ from sqlalchemy import func
 
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
