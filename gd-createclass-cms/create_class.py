4d53a142bd9804cb9d0e9810475b856c4d7243ba
vynious <shawnthiah@gmail.com>
2024-03-31 11:05:51 +0800

    update createclass-cms to have admin auth and updated kong and docker-compose file

--- gd-createclass-cms/create_class.py
+++ gd-createclass-cms/create_class.py
@@ -30,7 +30,7 @@ def authenticate(f):
         if not token:
             return jsonify({"error": "Missing Authorisation header"}), 401
 
-        validate_jwt_url = "http://user-ms:3005/api/users/validatejwt"
+        validate_jwt_url = "http://user-ms:3005/api/users/validatejwt/admin"
         headers = {"Authorisation": token}
         response = requests.get(validate_jwt_url, headers=headers)
 
