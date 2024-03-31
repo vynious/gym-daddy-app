6255daf5de5d4cc3fa9b54723d1bab583cd8dd56
ljlimjk <ljlimjk10@gmail.com>
2024-03-31 18:22:34 +0800

    remove uncessary comments

--- src/components/CardBooker.vue
+++ src/components/CardBooker.vue
@@ -1,76 +1,3 @@
-<!-- <template>
-	<div style="padding: 20px">
-		<a-row :gutter="16">
-			<a-col :span="8">
-				<a-card
-					title="Yoga"
-					:bordered="false"
-					style="font-family: 'Poppins Bold'"
-				>
-					<p style="font-family: 'Poppins Medium'">
-						A 1h session, this class will introduce the fundamentals
-						of a yoga practice, including breath, proper alignment,
-						and mindfulness practices.
-					</p>
-					<b>Difficulty: EASY</b>
-					<br />
-					<router-link to="/bookyoga">
-						<a-button
-							type="primary"
-							style="font-family: 'Poppins Medium'"
-							>Book Now</a-button
-						>
-					</router-link>
-				</a-card>
-			</a-col>
-			<a-col :span="8">
-				<a-card
-					title="Dance"
-					:bordered="false"
-					style="font-family: 'Poppins Bold'"
-				>
-					<p style="font-family: 'Poppins Medium'">
-						A 1h session where instructors will share their tips and
-						tricks accumulated from experience to teach the
-						trendiest dance moves.
-					</p>
-					<b>Difficulty: INTERMEDIATE</b>
-					<br />
-					<router-link to="/bookdance">
-						<a-button
-							type="primary"
-							style="font-family: 'Poppins Medium'"
-							>Book Now</a-button
-						>
-					</router-link>
-				</a-card>
-			</a-col>
-			<a-col :span="8">
-				<a-card
-					title="Pilates"
-					:bordered="false"
-					style="font-family: 'Poppins Bold'"
-				>
-					<p style="font-family: 'Poppins Medium'">
-						A 1h session where each class will work towards
-						balancing all muscle groups’ strength and flexibility,
-						stimulating the core muscles.
-					</p>
-					<b>Difficulty: HARD</b>
-					<br />
-					<router-link to="/bookpilates">
-						<a-button
-							type="primary"
-							style="font-family: 'Poppins Medium'"
-							>Book Now</a-button
-						>
-					</router-link>
-				</a-card>
-			</a-col>
-		</a-row>
-	</div>
-</template> -->
-
 <template>
 	<div style="padding: 20px">
 		<a-row :gutter="16">
@@ -122,7 +49,6 @@ export default {
 			axios
 				.get(baseURL)
 				.then((response) => {
-					// Assuming the server response directly contains an array of class objects
 					this.classes = response.data.classes;
 				})
 				.catch((error) => {
